39863988b5339d3f9665b873f05aa6ff24d2079b
dds1999 <dds1999@users.noreply.github.com>
2017-10-14 18:00:18 -0400

    All doors on the same line

--- spoopy.js
+++ spoopy.js
@@ -6,13 +6,13 @@ document.getElementById("door2").onclick = function() {myFunction2()};
 document.getElementById("door3").onclick = function() {myFunction3()};
 
 function myFunction() {
-    document.getElementById("door1").innerHTML = "<p><img src= \"opendoor2.jpg\" style='width:100px;height:200px;'></p>";
+    document.getElementById("door1").innerHTML = "<div><img src= \"opendoor2.jpg\" style='width:100px;height:200px;'></div>";
 }
 
 function myFunction2() {
-	document.getElementById("door2").innerHTML = "<p><img src= \"opendoor2.jpg\" style='width:100px;height:200px;'></p>";
+	document.getElementById("door2").innerHTML = "<div><img src= \"opendoor2.jpg\" style='width:100px;height:200px;'></div>";
 }
 
 function myFunction3() {
-	document.getElementById("door3").innerHTML = "<p><img src= \"opendoor2.jpg\" style='width:100px;height:200px;'></p>";
+	document.getElementById("door3").innerHTML = "<div><img src= \"opendoor2.jpg\" style='width:100px;height:200px;'></div>";
 }
