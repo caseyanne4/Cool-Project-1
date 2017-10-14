3bed1fb8bf2744ec41158e2be85e1e6954a63cb3
caseyanne4 <32784130+caseyanne4@users.noreply.github.com>
2017-10-14 15:43:03 -0400

    Add files via upload

--- spoopy.js
+++ spoopy.js
@@ -3,11 +3,16 @@
 }*/
 document.getElementById("door1").onclick = function() {myFunction()};
 document.getElementById("door2").onclick = function() {myFunction2()};
+document.getElementById("door3").onclick = function() {myFunction3()};
 
 function myFunction() {
     document.getElementById("door1").innerHTML = "<p><img src= \"opendoor2.jpg\" style='width:100px;height:200px;'></p>";
 }
 
 function myFunction2() {
-	document.getElementById("door2").innerHTML = "something";
+	document.getElementById("door2").innerHTML = "<p><img src= \"opendoor2.jpg\" style='width:100px;height:200px;'></p>";
+}
+
+function myFunction3() {
+	document.getElementById("door3").innerHTML = "<p><img src= \"opendoor2.jpg\" style='width:100px;height:200px;'></p>";
 }
