a821d85a4d0771d0ead41312e2ef600ae33a7b96
dds1999 <dds1999@users.noreply.github.com>
2017-10-14 21:07:13 -0400

    random js numbers

--- spoopy.js
+++ spoopy.js
@@ -1,9 +1,17 @@
-/*function clickFunc(){
-	document.getElementById("door1").innnerHITML = "CHANGE";
-}*/
-document.getElementById("door1").onclick = function() {myFunction()};
-document.getElementById("door2").onclick = function() {myFunction2()};
-document.getElementById("door3").onclick = function() {myFunction3()};
+var guess = false;
+
+function MontyHalloween() {
+    var correctDoor = Math.floor(Math.random() * 3) + 1;
+    /* I tested it and it workds */
+    console.log(correctDoor);
+    var correctDoorString = "door" + correctDoor;
+    console.log(correctDoorString);
+}
+
+window.onload = function() {
+    MontyHalloween();
+}
+
 
 function myFunction() {
     document.getElementById("door1").innerHTML = "<div><img src= \"opendoor2.jpg\" style='width:100px;height:200px;'></div>";
@@ -16,3 +24,7 @@ function myFunction2() {
 function myFunction3() {
 	document.getElementById("door3").innerHTML = "<div><img src= \"opendoor2.jpg\" style='width:100px;height:200px;'></div>";
 }
+
+document.getElementById("door1").onclick = function() {myFunction()};
+document.getElementById("door2").onclick = function() {myFunction2()};
+document.getElementById("door3").onclick = function() {myFunction3()};
